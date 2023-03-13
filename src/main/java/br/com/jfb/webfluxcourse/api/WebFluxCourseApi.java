d9ada3f9d5b6aea6f9064bacc526391aa22a5d89
Josivaldo Batista <josivaldo-fb@outlook.com>
2023-03-12 22:46:26 -0300

    Task02: Added WebFluxCourseApi interface

---
+++ src/main/java/br/com/jfb/webfluxcourse/api/WebFluxCourseApi.java
@@ -0,0 +1,22 @@
+package br.com.jfb.webfluxcourse.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface WebFluxCourseApi {
+
+  @PostMapping
+  ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
+  @GetMapping(value = "/{id}")
+  ResponseEntity<Mono<UserResponse>> findById(@PathVariable("id") String id);
+  @GetMapping
+  ResponseEntity<Flux<UserResponse>> findAll();
+  @PatchMapping(value = "/{id}")
+  ResponseEntity<Mono<UserResponse>> update(@PathVariable("id") String id, @RequestBody UserRequest request);
+  @DeleteMapping("/{id}")
+  ResponseEntity<Mono<Void>> delete(@PathVariable("id") String id);
+
+
+}
