ded057e3c0d4350df7a60047c1c64c6101eafab9
Josivaldo Batista <josivaldo-fb@outlook.com>
2023-03-14 20:02:34 -0300

    Task05: Added impl to update contract

--- src/main/java/br/com/jfb/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/jfb/webfluxcourse/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import br.com.jfb.webfluxcourse.model.request.UserRequest;
 import br.com.jfb.webfluxcourse.model.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
@@ -18,7 +19,8 @@ public interface UserMapper {
 
   @Mapping(target = "id", ignore = true)
   User toEntity(final UserRequest request);
-
+  @Mapping(target = "id", ignore = true)
+  User toEntity(final UserRequest request, @MappingTarget final User entity);
   UserResponse toResponse(final User entity);
 
 }
