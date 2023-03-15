ad2c398ed3994d94dff9a132fb4e47e23e38e082
Josivaldo Batista <josivaldo-fb@outlook.com>
2023-03-15 09:24:55 -0300

    Task05: Added unit test to update user service

--- src/test/java/br/com/jfb/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/jfb/webfluxcourse/service/UserServiceTest.java
@@ -72,4 +72,24 @@ class UserServiceTest {
 
     verify(repository, times(1)).findAll();
   }
+
+  @Test
+  void should_update_a_user() {
+    UserRequest request = new UserRequest(
+        "Maria Brown", "maria@email.com", "123");
+    User entity = User.builder().build();
+
+    when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+    when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+    when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+    Mono<User> result = service.update("123", request);
+
+    StepVerifier.create(result)
+        .expectNextMatches(user -> user.getClass() == User.class)
+        .expectComplete()
+        .verify();
+
+    verify(repository, times(1)).save(any(User.class)); //<- só para teste essa linha
+  }
 }
